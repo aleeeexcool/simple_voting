26e9ef088deaa44a31930443beb4c77b9c0b6a47
aleeeexcool <ramzhev.sasha@gmail.com>
2023-02-05 18:09:16 +0200

    changed the interface

--- contracts/IVoting.sol
+++ contracts/IVoting.sol
@@ -1,34 +1,34 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.7;
 
-contract IVoting {
+interface IVoting {
  
-    function addVoting(address[] calldata _candidates, uint _period) public {}
+    function addVoting(address[] calldata _candidates, uint _period) external;
 
-    function addCandidate(address _candidate, uint _id) public {}
+    function addCandidate(address _candidate, uint _id) external;
 
-    function deleteCandidate(address _candidate, uint _id) public {}
+    function deleteCandidate(address _candidate, uint _id) external;
 
-    function takePartInVoting(uint _id, address _candidate) public {}
+    function takePartInVoting(uint _id, address _candidate) external;
 
-    function withDrawMoney(uint _id) public {}
+    function withDrawMoney(uint _id) external;
 
-    function checkCandidate(address _candidate, uint _id) public view returns(bool) {}
+    function checkCandidate(address _candidate, uint _id) external view returns(bool);
 
-    function getVotingInfo(uint256 _votingID) public view returns (
+    function getVotingInfo(uint256 _votingID) external view returns (
             bool,
             uint256,
             uint256,
             uint256,
             uint256,
             address
-        ) {}
+        );
 
-    function startVoting(uint _id) public {}
+    function startVoting(uint _id) external;
 
-    function editVotingPeriod(uint _id, uint _newPeriod) public {}
+    function editVotingPeriod(uint _id, uint _newPeriod) external;
 
-    function setMaxAndMinCandidates(uint _maxCandidates) public {}
+    function setMaxAndMinCandidates(uint _maxCandidates) external;
 
     event candidateInfo(uint indexed id, address indexed candidate, bool existOnThisVoting);
 
--- contracts/Voting.sol
+++ contracts/Voting.sol
@@ -1,9 +1,7 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.7;
 
-import "./IVoting.sol";
-
-contract Voting is IVoting {
+contract Voting {
  
     address public owner;
     uint public counter;
@@ -38,4 +36,41 @@ contract Voting is IVoting {
         maxCandidates = _maxCandidates;
     }
 
+    function addVoting(address[] calldata _candidates, uint _period) public onlyOwner {
+        require(_candidates.length < maxCandidates, "Too many candidates");
+        Voting[counter].Period = _period; 
+        for( uint i = 0; i < _candidates.length; i++) {
+            addCandidate(counter, _candidates[i]);
+
+
+        }
+        emit votingDraftCreated(counter);
+        counter++;
+    }
+
+    function addCandidate(address _candidate, uint _id) public onlyOwner {
+        require(Voting[_id].started, "The voting has already begun!");
+        Voting[_id].Candidates[_candidate].isExistOnThisVoting = true;
+        emit candidateInfo(_id, _candidate, true);
+    }
+
+    function deleteCandidate(address _candidate, uint _id) public onlyOwner {
+        require(Voting[_id].started, "The voting has already begun!");
+        Voting[_id].Candidates[_candidate].isExistOnThisVoting = false;
+        emit candidateInfo(_id, _candidate, false);
+    }
+
+    function takePartInVoting(uint _id, address _candidate) public payable {
+        require(Voting[_id].started, "The voting doesn't start!");
+        require(Voting[_id].StartDate + Voting[_id].Period > block.timestamp, "The voting has ended!");
+
+
+    }
+
+
+    event candidateInfo(uint indexed id, address indexed candidate, bool existOnThisVoting);
+
+    event votingDraftCreated(uint indexed id);
+
+    event votingStarted(uint indexed id, uint startDate);
 }
