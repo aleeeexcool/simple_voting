960abcb44e443051a6191cbb447f5c06343a38ae
aleeeexcool <ramzhev.sasha@outlook.com>
2023-02-12 16:51:50 +0200

    deploy.js update

--- test/Voting.test.js
+++ test/Voting.test.js
@@ -2,7 +2,22 @@ const { time, loadFixture } = require("@nomicfoundation/hardhat-network-helpers"
 const { ethers } = require("hardhat");
 const { expect } = require("chai");
 
+describe("Voting", function () {
+    async function deploy() {
+        const [owner, user] = await ethers.getSigners();
+    
+        const Voting = await ethers.getContractFactory("Voting");
+        const myVoting = await Voting.deploy(2, 1);
+    
+        return { myVoting, owner, user };
+    }
 
+    it("Should set the right owner", async function () {
+        const { myVoting, owner } = await loadFixture(deploy);
+  
+        expect(await myVoting.owner()).to.equal(owner.address);
+      });
+});
 
 
 
