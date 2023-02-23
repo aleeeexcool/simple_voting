b728303072fb2af538f448fdfc69816c87e0a9d8
aleeeexcool <ramzhev.sasha@outlook.com>
2023-02-23 14:49:49 +0200

    add some new tests

--- test/Voting.test.js
+++ test/Voting.test.js
@@ -127,4 +127,88 @@ describe("Voting", function () {
 });
 
 
+// const { expect } = require("chai");
 
+// describe("Voting contract", function () {
+//   let voting;
+//   let owner;
+//   let candidate1;
+//   let candidate2;
+//   let candidate3;
+//   const maxCandidates = 3;
+//   const commission = 5; // 5% commission
+
+//   beforeEach(async function () {
+//     [owner, candidate1, candidate2, candidate3] = await ethers.getSigners();
+//     const Voting = await ethers.getContractFactory("Voting");
+//     voting = await Voting.deploy(maxCandidates, commission);
+//     await voting.deployed();
+//   });
+
+//   it("should initialize contract variables", async function () {
+//     expect(await voting.owner()).to.equal(owner.address);
+//     expect(await voting.counter()).to.equal(0);
+//     expect(await voting.minCandidates()).to.equal(2);
+//     expect(await voting.maxCandidates()).to.equal(maxCandidates);
+//     expect(await voting.Comission()).to.equal(commission);
+//   });
+
+//   it("should create a new voting with valid number of candidates", async function () {
+//     const candidates = [candidate1.address, candidate2.address];
+//     await expect(voting.addVoting(candidates, 7))
+//       .to.emit(voting, "votingDraftCreated")
+//       .withArgs(0);
+
+//     expect(await voting.checkCandidate(0, candidate1.address)).to.be.true;
+//     expect(await voting.checkCandidate(0, candidate2.address)).to.be.true;
+//     expect(await voting.checkCandidate(0, candidate3.address)).to.be.false;
+//   });
+
+//   it("should not create a new voting with invalid number of candidates", async function () {
+//     const candidates = [candidate1.address];
+//     await expect(voting.addVoting(candidates, 7)).to.be.revertedWith(
+//       "The number of candidates must comply with the voting rules!"
+//     );
+//   });
+
+//   it("should add and delete candidate successfully", async function () {
+//     const candidates = [candidate1.address, candidate2.address];
+//     await voting.addVoting(candidates, 7);
+
+//     await expect(voting.addCandidate(0, candidate3.address))
+//       .to.emit(voting, "candidateInfo")
+//       .withArgs(0, candidate3.address, true);
+
+//     await expect(voting.deleteCandidate(0, candidate3.address))
+//       .to.emit(voting, "candidateInfo")
+//       .withArgs(0, candidate3.address, false);
+
+//     expect(await voting.checkCandidate(0, candidate3.address)).to.be.false;
+//   });
+
+//   it("should start and end a voting successfully", async function () {
+//     const candidates = [candidate1.address, candidate2.address];
+//     await voting.addVoting(candidates, 7);
+
+//     await expect(voting.startVoting(0))
+//       .to.emit(voting, "votingStarted")
+//       .withArgs(0, await ethers.provider.getBlockNumber());
+
+//     const duration = 7;
+//     await ethers.provider.send("evm_increaseTime", [duration]);
+//     await ethers.provider.send("evm_mine", []);
+
+//     expect(await voting.takePartInVoting(0, candidate1.address, { value: 10 }))
+//       .to.emit(voting, "CandidateVoted")
+//       .withArgs(0, candidate1.address, 10);
+//   });
+
+//   it("should withdraw prize successfully", async function () {
+//     const candidates = [candidate1.address, candidate2.address];
+//     await voting.addVoting(candidates, 7);
+
+//     await expect(voting.startVoting(0))
+//       .to.emit(voting, "votingStarted")
+//       .withArgs(0, await ethers)
+// })
+// })
