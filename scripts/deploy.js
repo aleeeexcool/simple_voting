95ae7de8d3e4e0392f228cd76504e5de4363fa67
aleeeexcool <ramzhev.sasha@outlook.com>
2023-04-02 20:31:38 +0300

    add deploy.js update with front

--- scripts/deploy.js
+++ scripts/deploy.js
@@ -1,17 +1,57 @@
-const hre = require("hardhat");
+const hre = require('hardhat');
+const ethers = hre.ethers;
+const fs = require('fs');
+const path = require('path');
 
 async function main() {
-  const Voting = await hre.ethers.getContractFactory("Voting");
-  const myVoting = await Voting.deploy();
-
-  await myVoting.deployed();
-
-  console.log(
-    `Success!`
-  );
+    if (network.name === "hardhat") {
+        console.warn(
+        "You are trying to deploy a contract to the Hardhat Network, which" +
+        "gets automatically created and destroyed every time. Use the Hardhat" +
+        " option '--network localhost'"
+    );
 }
 
-main().catch((error) => {
-  console.error(error);
-  process.exitCode = 1;
-});
+    const [deployer] = await ethers.getSigners()
+
+    console.log("Deploying with", await deployer.getAddress())
+
+    const DutchAuction = await ethers.getContractFactory("DutchAuction", deployer)
+    const auction = await DutchAuction.deploy(
+        ethers.utils.parseEther('2.0'),
+        1,
+        "Motorbike"
+    )
+    await auction.deployed()
+
+    saveFrontendFiles({
+        DutchAuction: auction
+    })
+}
+
+function saveFrontendFiles(contracts) {
+    const contractsDir = path.join(__dirname, '/..', 'front/contracts')
+
+    if(!fs.existsSync(contractsDir)) {
+        fs.mkdirSync(contractsDir)
+    }
+
+Object.entries(contracts).forEach((contract_item) => {
+    const [name, contract] = contract_item
+
+    if(contract) {
+       fs.writeFileSync(
+        path.join(contractsDir, '/', name + '-contract-address.json'),
+        JSON.stringify({[name]: contract.address}, undefined, 2)
+        )
+    }
+
+    const ContractArtifact = hre.artifacts.readArtifactSync(name)
+
+    fs.writeFileSync(
+        path.join(contractsDir, '/', name + ".json"),
+        JSON.stringify(ContractArtifact, null, 2)
+        )
+    })
+}
+
